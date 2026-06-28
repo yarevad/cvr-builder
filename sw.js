11bb2280da865b58d90b19ba4820056d230d609b
Dave Ray Work <aiz_david.ray@Swervins-Mac-mini.local>
2026-06-28 13:27:27 +1000

    Fix SW update: skipWaiting + clients.claim to take over immediately

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE = 'cvr-builder-v3';
+const CACHE = 'cvr-builder-v4';
 const ASSETS = ['/cvr-builder/', '/cvr-builder/index.html', '/cvr-builder/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
