d0524801081052638b3a31f76bf8019b7b07fa31
Dave Ray Work <aiz_david.ray@Swervins-Mac-mini.local>
2026-06-28 13:37:58 +1000

    Add text detail to 'opportunities' IQ sub-option; bump cache v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cvr-builder-v5';
+const CACHE = 'cvr-builder-v6';
 const ASSETS = ['/cvr-builder/', '/cvr-builder/index.html', '/cvr-builder/manifest.json'];
 
 self.addEventListener('install', e => {
